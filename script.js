3c7f4bb976bc5c1170e46d32956beb57e3e6789f
HeshamAbdelalem <hesham.abdelalem1@gmail.com>
2023-08-21 16:05:52 +0300

    try to fix a bug

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
 let searchInput = document.querySelector('#searchInput');
 let todayTemp = document.querySelector('.tempToday');
+let feelsLike = document.querySelector('.feelsLike');
 let city = document.querySelector('.city');
 let region = document.querySelector('.region');
 let country = document.querySelector('.country');
 let userLatitude;
 let userLongitude;
+let finalWeather;
+let userInput;
 
 // !Geo location
 
-if (navigator.geolocation) {
-  navigator.geolocation.getCurrentPosition(showPosition);
+// if (navigator.geolocation) {
+//   navigator.geolocation.getCurrentPosition(showPosition);
 
-  console.log('location works 🎉✨');
-} else {
-  console.log('geolocation not supported by your browser');
-}
+//   console.log('location works 🎉✨');
+// } else {
+//   console.log('geolocation not supported by your browser');
+// }
 
 function showPosition(position) {
   userLatitude = position.coords.latitude;
@@ -23,32 +26,46 @@ function showPosition(position) {
   console.log(userLatitude, userLongitude);
 }
 
+async function getLocation() {
+  return new Promise(function () {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(showPosition);
+      console.log('location works 🎉✨');
+    } else {
+      console.log('geolocation not supported by your browser');
+    }
+  });
+}
+
 // ! fetch api
-async function getUserCurrentWeather(lat = 30.0994522, long = 31.3328633) {
-  console.log(userLatitude, userLongitude);
+async function getUserCurrentWeather() {
+  console.log('this is the user current weather func');
+  await getLocation();
 
   let weather = await fetch(
-    `http://api.weatherapi.com/v1/current.json?key=1689e76bab55400991481619231508&q=${lat},${long}`
+    `http://api.weatherapi.com/v1/current.json?key=1689e76bab55400991481619231508&q=${userInput}`
   );
+  displayHtml();
+  console.log(weather.json());
   if (weather.status == 200) {
     console.log('status 200');
-    console.log(weather.json());
+    console.log('from getusercurrentweather func', userLatitude, userLongitude);
   }
 }
 
-async function searchWeather(location) {
-  let weather = await fetch(
-    `https://api.weatherapi.com/v1/current.json?key=1689e76bab55400991481619231508&q=${location}`
-  );
-}
-
 searchInput.addEventListener('input', async function () {
-  let userInput = this.value;
+  userInput = searchInput.value || `${userLatitude},${userLongitude}`;
+
   let weather = await fetch(
     `https://api.weatherapi.com/v1/forecast.json?key=1689e76bab55400991481619231508&q=${userInput}&days=3`
   );
-  let finalWeather = await weather.json();
+  finalWeather = await weather.json();
 
+  console.log('final weather:', finalWeather);
+  displayHtml();
+});
+
+function displayHtml() {
   // # day 0
 
   todayTemp.innerHTML = finalWeather.current.temp_c;
@@ -58,18 +75,25 @@ searchInput.addEventListener('input', async function () {
   document
     .querySelector('.day-0 .today-icon')
     .setAttribute('src', finalWeather.current.condition.icon);
-  city.innerHTML = finalWeather.location.name;
-  region.innerHTML = finalWeather.location.region;
+  feelsLike.innerHTML = finalWeather.current.feelslike_c;
+  city.innerHTML = finalWeather.location.name + ', ';
+  region.innerHTML = finalWeather.location.region + ', ';
   country.innerHTML = finalWeather.location.country;
 
   //  #day 1
   document.querySelector('.day-1 .tempToday').innerHTML =
     finalWeather.forecast.forecastday[1].day.avgtemp_c;
   document.querySelector('.day-1 .city').innerHTML = city.innerHTML;
-  //   document.querySelector('.day-1 .region') = city.innerHTML;
-  //   document.querySelector('.day-1 .country');
+  // document.querySelector('.day-1 .region') = city.innerHTML;
 
   // #day 2
+}
 
-  console.log(finalWeather);
-});
+async function doAll() {
+  await getLocation();
+  await getUserCurrentWeather();
+}
+
+doAll();
+
+getUserCurrentWeather();
